7483e3005150562b33d1b0844fce6d91126f8f40
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-06-12 18:17:36 +0530

    Redirect from login to Main Landing Screen

--- app/src/main/java/com/bookyard/booky/ui/activities/LoginActivity.java
+++ app/src/main/java/com/bookyard/booky/ui/activities/LoginActivity.java
@@ -1,8 +1,16 @@
 package com.bookyard.booky.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.bookyard.booky.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Rishabh Bhatia on 6/12/2016.
@@ -10,10 +18,20 @@ import com.bookyard.booky.R;
 public class LoginActivity extends BookyActivity {
 
 
+    @BindView(R.id.et_login_phone_num)
+    EditText etLoginPhoneNum;
+    @BindView(R.id.et_login_pass)
+    EditText etLoginPass;
+    @BindView(R.id.bt_login_action)
+    Button btLoginAction;
+    @BindView(R.id.bt_login_sign_up)
+    Button btLoginSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -30,4 +48,25 @@ public class LoginActivity extends BookyActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    @OnClick({R.id.bt_login_action, R.id.bt_login_sign_up})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_login_action:
+                launchActivity();
+                break;
+            case R.id.bt_login_sign_up:
+                launchActivity();
+                break;
+        }
+    }
+
+    private void launchActivity()
+    {
+        Intent intent = new Intent(LoginActivity.this, MainLandingActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_from_bottom, R.anim.slide_out_to_bottom);
+        finish();
+    }
+
 }
