ea95b08dd64c0523467f77f51b1d19611343d526
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-06-26 17:56:03 +0530

    Login Screen Design changes

--- app/src/main/java/com/bookyard/booky/ui/activities/LoginActivity.java
+++ app/src/main/java/com/bookyard/booky/ui/activities/LoginActivity.java
@@ -3,14 +3,15 @@ package com.bookyard.booky.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.bookyard.booky.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import mehdi.sakout.fancybuttons.FancyButton;
 
 /**
  * Created by Rishabh Bhatia on 6/12/2016.
@@ -23,15 +24,32 @@ public class LoginActivity extends BookyActivity {
     @BindView(R.id.et_login_pass)
     EditText etLoginPass;
     @BindView(R.id.bt_login_action)
-    Button btLoginAction;
-    @BindView(R.id.bt_login_sign_up)
-    Button btLoginSignUp;
+    FancyButton btLoginAction;
+    @BindView(R.id.tv_login_sign_up)
+    TextView tvLoginSignUp;
+    @BindView(R.id.tv_login_forgot_pass)
+    TextView tvLoginForgotPass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        measureLoginButton();
+    }
+
+    private void measureLoginButton()
+    {
+        etLoginPhoneNum.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        etLoginPass.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+
+        etLoginPass.setWidth(etLoginPhoneNum.getMeasuredWidth());
+        etLoginPass.setHeight(etLoginPhoneNum.getMeasuredHeight());
+
+        btLoginAction.setMinimumWidth(etLoginPhoneNum.getMeasuredWidth());
+        //btLoginAction.setMinimumHeight(etLoginPhoneNum.getMeasuredHeight());
     }
 
     @Override
@@ -49,20 +67,16 @@ public class LoginActivity extends BookyActivity {
         super.onDestroy();
     }
 
-    @OnClick({R.id.bt_login_action, R.id.bt_login_sign_up})
+    @OnClick(R.id.bt_login_action)
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_login_action:
                 launchActivity();
                 break;
-            case R.id.bt_login_sign_up:
-                launchActivity();
-                break;
         }
     }
 
-    private void launchActivity()
-    {
+    private void launchActivity() {
         Intent intent = new Intent(LoginActivity.this, MainLandingActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in_from_bottom, R.anim.slide_out_to_bottom);
